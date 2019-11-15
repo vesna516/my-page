ed2e70253b7dbc472dcd0dca7984a9a64e6ccf70
Lyudmila Zaytseva <vesna516@yandex.ru>
2019-11-15 10:20:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Resume page
+
+### Install
+
+Node version: 11.14.0
+
+Building styles:
+
+    npm run build
+
+### Watch styles
+
+    npm run watch
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const gulp = require('gulp');
+const prefixer = require('gulp-autoprefixer');
+const cssmin = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const del = require('del');
+
+const path = {
+    src: {
+        html: 'src/*.html',
+        style: 'src/styles.scss',
+        images: 'src/images/*.svg',
+    },
+    watch: {
+        html: 'src/*.html',
+        style: 'src/scss/*.scss',
+    },
+    build: {
+        html: 'docs/',
+        css: 'docs/',
+        images: 'docs/images/',
+    },
+    clean: './docs'
+};
+
+function clean() {
+    return del([path.clean]);
+}
+
+function html() {
+    return gulp.src(path.src.html)
+        .pipe(gulp.dest(path.build.html));
+}
+
+function styles() {
+    return gulp.src(path.src.style)
+        .pipe(sass())
+        .pipe(prefixer())
+        .pipe(concat('app.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest(path.build.css));
+}
+
+function images(done) {
+    gulp.src(path.src.images)
+        .pipe(gulp.dest(path.build.images));
+    done();
+}
+
+function watch() {
+    gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.style, styles);
+}
+
+var build = gulp.series(clean, html, styles, images);
+
+exports.build = build;
+exports.watch = watch;
